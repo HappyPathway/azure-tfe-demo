cf40d126349d43fbf6696876e90c066357475ade
Dave Arnold <darnold@hashicorp.com>
2018-09-06 15:01:09 -0700

    works, now committing

--- setup/organization/outputs.tf
+++ setup/organization/outputs.tf
@@ -1,15 +1,7 @@
-output "vcs_id" {
-    value = "${tfe_organization_vcs.github.vcs_id}"
-}
-
 output "token" {
     value = "${tfe_organization_token.token.token}"
 }
 
 output "id" {
     value = "${tfe_organization_token.token.id}"
-}
-
-output "callback_url" {
-    value = "${tfe_organization_vcs.github.callback_url}"
 }
